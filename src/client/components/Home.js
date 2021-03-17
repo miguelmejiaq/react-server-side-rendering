4f6acf6bfc366f5a21b54ea8e972204033188b45
miguelmejiaq <miguelmejiaq@outlook.com>
2021-03-16 23:39:38 -0500

    Adding full server configuration and client.js file

--- src/client/components/Home.js
+++ src/client/components/Home.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Home = () => {
-    return <div>Hello from home component</div>
+    return (
+        <div>
+            <div>Hello from home component</div>
+            <button onClick={() => console.log('button pressed!')}>Press me!</button>
+        </div>
+    );
 };
 
 export default Home;
